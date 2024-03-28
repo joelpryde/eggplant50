81cad06dc10b3ba2d743cb0a0f6e6a755476b9d8
Joel Pryde <joelpryde@gmail.com>
2024-03-28 13:13:41 +0100

    Add win state

--- games/CGOL/CrateParent.gd
+++ games/CGOL/CrateParent.gd
@@ -1,51 +1,15 @@
 extends Node2D
 
 onready var crate_prefab = load("res://games/CGOL/Crate.tscn")
-onready var fill_prefab = load("res://games/CGOL/Fill.tscn")
+onready var goal_prefab = load("res://games/CGOL/Fill.tscn")
 onready var constants = load("res://games/CGOL/Constants.gd")
 onready var current_level_path = "res://games/CGOL/assets/levels/level1.csv"
-onready var audio_player = get_parent().get_node("AudioStreamPlayer")
+onready var pop_player = get_parent().get_node("PopPlayer")
+onready var win_player = get_parent().get_node("WinPlayer")
 onready var player = get_parent().get_node("Player")
-onready var fill_parent = get_parent().get_node("FillParent")
+onready var goal_parent = get_parent().get_node("FillParent")
 var tweens_completed = true
-var filled = {}
-
-func create_box_level():
-	var create_crate_start = 2
-	var create_crate_end = 5
-	for i in range(create_crate_start, create_crate_end+1):
-		create_crate(create_crate_start, i)
-		create_crate(create_crate_end, i)
-		create_crate(i, create_crate_start)
-		create_crate(i, create_crate_end)
-
-    # left top
-	create_goal(2,1)
-	create_goal(2,2)
-	create_goal(1,2)
-
-	# right top
-	create_goal(5,1)
-	create_goal(5,2)
-	create_goal(6,2)
-
-	# bottom left
-	create_goal(2,6)
-	create_goal(2,5)
-	create_goal(1,5)
-
-	# bottom right
-	create_goal(5,6)
-	create_goal(5,5)
-	create_goal(6,5)
-
-func create_cross_level():
-	create_crate(3, 4)
-	create_crate(4, 4)
-	create_crate(5, 3)
-	create_goal(4, 3)
-	create_goal(4, 4)
-	create_goal(4, 5)
+var goals = {}
 
 func read_level_file(path):
 	var data = []
@@ -103,7 +67,7 @@ func build_grid():
 	return grid
 	
 func create_crate(x, y):
-	print("Creating crate at " + str(x) + ", " + str(y))
+	# print("Creating crate at " + str(x) + ", " + str(y))
 	var crate = crate_prefab.instance()
 	add_child(crate)
 	crate.position = Vector2(x * constants.grid_cell_size, y * constants.grid_cell_size)
@@ -111,14 +75,14 @@ func create_crate(x, y):
 	crate.y = y
 		
 func create_goal(x, y):
-	print("Creating fill at " + str(x) + ", " + str(y))
-	if filled.has(position_key(x, y)):
+	# print("Creating goal at " + str(x) + ", " + str(y))
+	if goals.has(position_key(x, y)):
 		return
-	filled[position_key(x, y)] = true
-	var fill = fill_prefab.instance()
-	fill.z_index = 1
-	fill_parent.add_child(fill)
-	fill.position = Vector2(x * constants.grid_cell_size, y * constants.grid_cell_size)
+	goals[position_key(x, y)] = true
+	var goal = goal_prefab.instance()
+	goal.z_index = 1
+	goal_parent.add_child(goal)
+	goal.position = Vector2(x * constants.grid_cell_size, y * constants.grid_cell_size)
 	
 func _process(_delta):
 	if Input.is_action_just_pressed("action1"):
@@ -129,9 +93,9 @@ func _process(_delta):
 		
 func clear_grid():
 	print("resetting grid")
-	for fill in fill_parent.get_children():
+	for fill in goal_parent.get_children():
 		fill.queue_free()
-	filled = {}
+	goals = {}
 	for childCrate in get_children():
 		childCrate.queue_free()
 
@@ -139,34 +103,28 @@ func get_neighbors(grid, x, y):
 	var neighbors = []
 	for i in range(-1, 2):
 		for j in range(-1, 2):
-
 			if i == 0 and j == 0:
 				continue
-
 			var key = position_key(x + i, y + j)
 			if (grid.has(key)):
 				neighbors.append(grid[key])
-				
 	return neighbors
 				
 func run_next_step():
 	print("Running next step")
-	audio_player.play()
+	pop_player.play()
 	var grid = build_grid()
-	
 	for x in range(0, constants.grid_size+1):
 		for y in range(0, constants.grid_size+1):
-
 			if (x < 1 or x >= constants.grid_size or y < 1 or y > constants.grid_size):
 				continue
-
 			var neighbors = get_neighbors(grid, x, y)
-			
 			if neighbors.size() < 2 or neighbors.size() > 3:	# Death case
 				var key = position_key(x, y)
 				if (grid.has(key)):
 					print("Killing crate at " + str(x) + ", " + str(y))
 					grid[key].queue_free()
+					remove_child(grid[key])
 			else:
 				if neighbors.size() == 3: # Birth case
 					# Don't create crate if player is in this space!
@@ -174,10 +132,10 @@ func run_next_step():
 					if player_pos[0] == x and player_pos[1] == y:
 						print("Player is in this space")
 						continue
-
 					var key = position_key(x, y)
 					if (!grid.has(key)):
 						create_crate(x, y)
+	call_deferred("check_win")
 
 func tweenCompleted(_crate, tween):
 	print("all completed")
@@ -186,8 +144,19 @@ func tweenCompleted(_crate, tween):
 		if childCrate is Crate:
 			if (childCrate.is_pushing):
 				childCrate.reset_pushing()
-
 	tween.queue_free()
-
 	run_next_step()
+
+func check_win():
+	print("Checking win")
+	var grid = build_grid()
+	if (grid.size() != goals.size()):
+		return
+	for key in goals.keys():
+		if (!grid.has(key)):
+			return
+	print("You win!")
+	win_player.play()
+
+
 
